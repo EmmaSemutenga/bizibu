26b64a5d8a2cc6acd048813e0984fd774811abcf
Emmanuel Semutenga <emzsema@gmail.com>
2021-04-08 14:55:14 +0300

    added post model

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Article(models.Model):
     name = models.CharField(max_length=10, null=True, blank=True)
+
+class Post(models.Model):
+    name = models.CharField(max_length=10, null=True, blank=True)
